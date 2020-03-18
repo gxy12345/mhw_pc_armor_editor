fb3a545060582747c8fcf528b114133de7f5a5a7
XiayuGuo <xiayuguo@XiayuGuodeMacBook-Pro.local>
2020-03-18 18:37:07 +0800

    modify variables and armor data

--- script/auto_modify_hex.py
+++ script/auto_modify_hex.py
@@ -27,24 +27,24 @@ def main_process():
     conf = read_config()
     print('load %s armor data to change' % len(conf))
     with open('../file/armor.am_dat', 'rb') as f:
-        datas = f.read()
-        a = binascii.b2a_hex(datas)
-        a_str = a.decode()
+        byte_data = f.read()
+        byte_s = binascii.b2a_hex(byte_data)
+        hex_str = byte_s.decode()
         for armor in conf:
             # print(armor)
             if len(armor['target']) != len(armor['value']):
                 print("[warning] value %s length not match %s!" % (armor['value'], armor['target']))
                 continue
-            if a_str.find(armor['target']) != -1:
-                a_str = re.sub(armor['target'], armor['value'], a_str)
+            if hex_str.find(armor['target']) != -1:
+                hex_str = re.sub(armor['target'], armor['value'], hex_str)
                 print("%s has been successfully changed." % armor['comment'])
             else:
                 print("cannot find armor %s, please check if the hex value is correct" % armor['comment'])
-        a_byte = a_str.encode()
-        new_a = binascii.a2b_hex(a_byte)
+        hex_byte = hex_str.encode()
+        new_byte_data = binascii.a2b_hex(hex_byte)
 
         with open('../output/armor.am_dat', 'wb') as f2:
-            f2.write(new_a)
+            f2.write(new_byte_data)
         print('success')
 
 if __name__ == '__main__':
